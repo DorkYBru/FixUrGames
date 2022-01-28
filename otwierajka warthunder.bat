df8ad2db29bd680e8b0574a0c6895b868783a977
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-01-28 06:57:41 +0000

    added wt skin tutorial pl

---
+++ otwierajka warthunder.bat
@@ -0,0 +1,8 @@
+@echo off
+echo "wlacz wt i kliknij na ta probowke tak jak on na tym filmiku, wtedy ci sie pojawi pojazd z napisami sample"
+set /p tak="Zrestartuj WT jak to zrobisz przejdz dalej.Aby przejsc dalej napisz cokolwiek"
+mkdir UserSkins
+start .\UserSkins
+echo "w folderze ktory sie otworzyl powinien byc folder o nazwie twojego czolgu"
+set /p tak="Aby przejsc dalej napisz cokolwiek"
+echo "teraz mozesz go edytowac!"

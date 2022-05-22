32fabb0165bf2edf5e2dca99cd4713ae133836f6
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-05-22 17:35:51 +0200

    added minecraft lag fix

---
+++ ping.bat
@@ -0,0 +1,19 @@
+@echo off
+
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+wmic process where name="svchost.exe" CALL setpriority "idle"
+ping 127.0.0.1 -n 5 >nul
+wmic process where name="javaw.exe" CALL setpriority "high priority"
+wmic process where name="javaw.exe" CALL setpriority "high priority"
+sc start BITS
+goto LOGO
+:LOGO
+cls
+color 0b
+echo "No Lag <3"
+start https://dorkybru.github.io/FixUrGames/ty.html
+pause

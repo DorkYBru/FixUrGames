27c1dcc8abc98192e1bba1616aa68fb6b35b88c9
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-08-30 20:35:29 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,9 +5,10 @@ curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/DisableCortana.p
 curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/DisableDiagTrack.ps1 -o ddt.ps1
 curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/DisableSmartScreen.ps1 -o dss.ps1
 curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/DisableTelemetry.ps1 -o dt.ps1
-curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/FPS%20Boost.reg -o fps.reg
+curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/FPS-Boost.reg -o fps.reg
 curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/OnlineGamesBoost.reg -o ogb.reg
 curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/UninstallMsftBloat.ps1 -o umb.ps1
+curl https://raw.githubusercontent.com/DorkYBru/FixUrGames/main/FasterPC-Shutdown.reg -o fpss.reg
 start dai.ps1
 start dca.ps1
 start dc.ps1
@@ -17,6 +18,7 @@ start dt.ps1
 start fps.reg
 start ogb.reg
 start umb.ps1
+start fpss.reg
 
 
 
